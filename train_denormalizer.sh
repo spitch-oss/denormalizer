3ad29119eb57b72ebd8264c4e2b992ee51739170
Benjamin Suter <benjamin.suter@spitch.ch>
2021-08-30 19:57:30 +0200

    correct typo in training script

--- train_denormalizer.sh
+++ train_denormalizer.sh
@@ -47,7 +47,7 @@ force_overwrite=${force_overwrite:-false}
 
 if [ ${size} = large ]; then
   arch=transformer_normalizer
-else if [ ${size} = small ]; then
+elif [ ${size} = small ]
   arch=transformer_small
 else
   echo "Invalid architecture. Choose between 'large' and 'small'."
